c6f19c445d8833a019a3820d7a0d011324d4eae7
Tore Fadnes <tore.fadnes@ikamr.no>
2019-08-22 09:35:50 +0200

    Changed form textbox size

--- KDRS_SIARD_Extdoc/Form1.Designer.cs
+++ KDRS_SIARD_Extdoc/Form1.Designer.cs
@@ -115,7 +115,7 @@
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(598, 78);
+            this.textBox1.Size = new System.Drawing.Size(519, 91);
             this.textBox1.TabIndex = 7;
             // 
             // backgroundWorker1
@@ -126,7 +126,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(636, 480);
+            this.ClientSize = new System.Drawing.Size(557, 493);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.btnChooseExtDoc);
             this.Controls.Add(this.txtExtDocFolder);
